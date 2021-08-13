b8f5db640f9e777f8e42217ac0ccff4d0438f374
JuanitaCeci <juanitaceci0624@gmail.com>
2021-08-13 11:28:41 -0400

    first commit

---
+++ src/gitHomeworkPack/FirstGitHW.java
@@ -0,0 +1,24 @@
+package gitHomeworkPack;
+
+public class FirstGitHW {
+
+	public static void main(String[] args) {
+
+		System.out.println("------- Integers 1-10 ----------");
+
+		for (int i = 0; i <= 10; i++) {
+			System.out.print(i + " ");
+		}
+
+		System.out.println("-------- Integers 1-100 --------");
+		
+		
+	
+	
+	
+	
+	
+	
+	}
+
+}
